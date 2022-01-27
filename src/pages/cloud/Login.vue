7765cd7c535d628f6917f442b489bbbdea3ab277
Hansin1997 <845612500@qq.com>
2022-01-28 02:50:47 +0800

    [ADD] Add @dustlight/auth-ext

---
+++ src/pages/cloud/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page padding>
+    <!-- OAuth2 Implict 授权模式  -->
+    <auth-login
+      :index="{
+        path: 'test1',
+      }"
+      :help="{
+      path:'/help'
+      }"
+    >
+    </auth-login>
+  </q-page>
+</template>
+
+<style lang="sass" scoped>
+.directive-target
+  width: 50px
+  height: 50px
+</style>
