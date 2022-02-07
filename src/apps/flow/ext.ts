4694a485914f21f5d1b1fdd5a81db79e6ba6c438
Hansin1997 <845612500@qq.com>
2022-02-07 16:58:33 +0800

    UserTask deployment

--- src/apps/flow/ext.ts
+++ src/apps/flow/ext.ts
@@ -1,6 +1,15 @@
-import {Configuration, ProcessesApi, InstancesApi, MessagesApi, TriggersApi,UserTasksApi} from '@dustlight/flow-client-axios-js'
+import {
+  Configuration,
+  ProcessesApi,
+  InstancesApi,
+  MessagesApi,
+  TriggersApi,
+  UserTasksApi
+} from '@dustlight/flow-client-axios-js'
+import {Configuration as DConfiguration, FormsApi} from '@dustlight/datacenter-client-axios-js'
 
 const endpoint = "https://flow.dustlight.cn"
+const dcEndpoint = "https://datacenter.dustlight.cn"
 
 declare interface Ext {
   basePath: string
@@ -9,6 +18,7 @@ declare interface Ext {
   messagesApi: (token: any) => MessagesApi,
   triggersApi: (token: any) => TriggersApi,
   userTasksApi: (token: any) => UserTasksApi,
+  formsApi: (token: any) => FormsApi,
   getStatusColor: (status: string) => string,
   getStatusIcon: (status: string) => string
 }
@@ -20,6 +30,10 @@ const ext: Ext = {
   messagesApi: (token: any) => new MessagesApi(new Configuration({basePath: endpoint, accessToken: token})),
   triggersApi: (token: any) => new TriggersApi(new Configuration({basePath: endpoint, accessToken: token})),
   userTasksApi: (token: any) => new UserTasksApi(new Configuration({basePath: endpoint, accessToken: token})),
+  formsApi: (token: any) => new FormsApi(new DConfiguration({
+    basePath: dcEndpoint,
+    accessToken: token
+  })),
   getStatusColor(status) {
     switch (status) {
       case 'ACTIVE':
--- src/apps/flow/pages/UserTask.vue
+++ src/apps/flow/pages/UserTask.vue
@@ -3,6 +3,37 @@
     :title="$appt('menu.userTasks')"
     v-slot="{client,user,token}">
     {{ "", client_ = client, user_ = user, token_ = token }}
+
+
+    <div class="q-pb-md" v-if="loading.task || !task">
+      <q-skeleton width="2em"/>
+    </div>
+    <div class="q-pb-md text-subtitle1" v-else>
+      <q-icon size="2em" :name="task.completedAt ? 'check' :  'person'"/>
+      <q-btn flat dense no-caps
+             target="_blank"
+             :to="{name: $options.app + '/' + 'process',params:{name: task.processName}}">
+        {{ task.processName }}
+      </q-btn>
+      <span> / </span>
+      <span class="text-caption">{{ task.id }}</span>
+    </div>
+
+    <div class="q-pb-md" v-if="loading.task">
+      <q-spinner-gears size="4em"/>
+    </div>
+    <div class="q-pb-md" v-else>
+
+      {{ task }}
+    </div>
+
+    <div class="q-pb-md" v-if="loading.form">
+      <q-spinner-gears size="4em"/>
+    </div>
+    <div class="q-pb-md" v-else-if="form">
+      <json-form :schema="form.schema"/>
+    </div>
+
   </client-required-adaptive-layout>
 </template>
 
@@ -17,7 +48,72 @@ export default {
       user_: null,
       token_: null,
       client_: null,
+      taskId: this.$route.params.id,
+      task: null,
+      form: null,
+      loading: {
+        task: false,
+        form: false
+      }
     }
+  },
+  computed: {
+    /**
+     *
+     * @returns {UserTasksApi}
+     */
+    userTasksApi() {
+      return this.$options.ext.userTasksApi(this.token_.access_token)
+    },
+    /**
+     *
+     * @returns {FormsApi}
+     */
+    formsApi() {
+      return this.$options.ext.formsApi(this.token_.access_token)
+    }
+  },
+  watch: {
+    // taskId() {
+    //   this.$router.push({name: this.$route.name, params: {id: this.taskId}})
+    // },
+    // "$route.params.id"() {
+    //   this.taskId = this.$route.params.id
+    //   this.loadTask()
+    // }
+    token_() {
+      this.loadTask()
+    },
+    client_() {
+      this.loadTask()
+    }
+  },
+  methods: {
+    loadForm(formName) {
+      if (this.loading.form || this.client_ == null || this.token_ == null)
+        return
+      this.loading.form = true
+      this.formsApi.getLatestForm(formName, this.client_.cid)
+        .then(res => this.form = res.data)
+        .catch(this.$throw)
+        .finally(() => this.loading.form = false)
+    },
+    loadTask() {
+      if (this.loading.task || this.client_ == null || this.token_ == null)
+        return
+      this.loading.task = true
+      this.form = null
+      this.userTasksApi.getUserTask(this.taskId, this.client_.cid)
+        .then(res => {
+          this.task = res.data
+          this.loadForm(this.task.form)
+        })
+        .catch(this.$throw)
+        .finally(() => this.loading.task = false)
+    }
+  },
+  mounted() {
+    this.loadTask()
   }
 }
 </script>
