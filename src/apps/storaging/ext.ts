1c80c364512f5cae0fcb77e32176c7d32851fb8c
Hansin1997 <845612500@qq.com>
2022-02-17 17:42:07 +0800

    Objects

--- src/apps/storaging/ext.ts
+++ src/apps/storaging/ext.ts
@@ -1,3 +1,39 @@
-export default {
+import {
+  Configuration,
+  ConfigurationsApi,
+  ObjectsApi
+} from '@dustlight/storaging-client-axios-js'
 
+import {UsersApi} from "@dustlight/auth-client-axios-js"
+
+const endpoint = "https://storaging.dustlight.cn"
+// @ts-ignore
+const authEndpoint = process.env.auth["config.apiEndpoint"]
+const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+function bytesToSize(len: number | undefined | null) {
+  if(len == undefined || null){
+    return "-"
+  }
+  if (len === 0) return '0 B';
+  let i = Math.floor(Math.log(len) / Math.log(1024));
+  return (len / Math.pow(1024, i)).toPrecision(3) + ' ' + sizes[i];
 }
+
+declare interface Ext {
+  basePath: string
+  configsApi: (token: any) => ConfigurationsApi
+  objectsApi: (token: any) => ObjectsApi,
+  usersApi: (token: any) => UsersApi
+  byteString: (len: number | undefined | null) => string
+}
+
+const ext: Ext = {
+  basePath: endpoint,
+  configsApi: (token: any) => new ConfigurationsApi(new Configuration({basePath: endpoint, accessToken: token})),
+  objectsApi: (token: any) => new ObjectsApi(new Configuration({basePath: endpoint, accessToken: token})),
+  usersApi: (token: any) => new UsersApi(new Configuration({basePath: authEndpoint, accessToken: token})),
+  byteString: bytesToSize
+}
+
+export default ext
--- src/apps/storaging/pages/Objects.vue
+++ src/apps/storaging/pages/Objects.vue
@@ -4,6 +4,54 @@
     v-slot="{client,user,token}">
     {{ "", client_ = client, user_ = user, token_ = token }}
 
+    <q-list separator v-if="loading">
+      <q-item v-for="i in size" :key="i">
+        <q-item-section avatar>
+          <q-icon name="text_snippet"/>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>
+            <q-skeleton type="text" width="2em"/>
+          </q-item-label>
+          <q-item-label caption>
+            <q-skeleton type="text" width="3em"/>
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+    <q-list style="text-overflow: ellipsis" separator v-else-if="objects && objects.data.length > 0">
+      <q-item v-for="(obj,index) in objects.data" :key="obj" clickable v-ripple>
+        <q-item-section avatar>
+          <q-icon name="text_snippet"/>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label lines="1">
+            {{ obj.name }}
+          </q-item-label>
+          <q-item-label caption lines="1">
+            {{ obj.description }}
+          </q-item-label>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label caption :lines="1">
+            <q-badge :label="obj.type || '-'"/>
+          </q-item-label>
+          <q-item-label lines="1" caption>
+            {{ $moment(obj.updatedAt) }}
+          </q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <q-item-label lines="1" caption>
+            {{ $options.ext.byteString(obj.size) }}
+          </q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <q-icon name="keyboard_arrow_right"/>
+        </q-item-section>
+      </q-item>
+    </q-list>
+    <no-results v-else/>
+
     <q-page-sticky :offset="[18,18]">
       <q-btn :loading="creating" @click="newObject" color="primary" round icon="add"/>
     </q-page-sticky>
@@ -13,20 +61,67 @@
 <script>
 import ChannelList from "../../messenger/components/ChannelList";
 import ClientRequiredAdaptiveLayout from "../../../components/container/ClientRequiredAdaptiveLayout";
+import NoResults from "../../../components/common/NoResults";
 
 export default {
   name: "Objects",
-  components: {ChannelList, ClientRequiredAdaptiveLayout},
+  components: {NoResults, ChannelList, ClientRequiredAdaptiveLayout},
   data() {
     return {
       client_: null,
       user_: null,
       token_: null,
-      creating: false
+      creating: false,
+      objects: null,
+      loading: false,
+      key: this.$route.query.key || "",
+      page: this.$route.query.page || 1,
+      size: 10
     }
   },
-  methods:{
-    newObject(){
+  computed: {
+    /**
+     *
+     * @returns {ObjectsApi}
+     */
+    objectsApi() {
+      return this.$options.ext.objectsApi(this.token_.access_token)
+    },
+    /**
+     *
+     * @returns {UsersApi}
+     */
+    usersApi() {
+      return this.$options.ext.usersApi(this.token_.access_token)
+    }
+  },
+  watch: {
+    token_() {
+      this.page = this.$route.query.page || 1
+      this.loadObjects()
+    },
+    client_() {
+      this.page = this.$route.query.page || 1
+      this.loadObjects()
+    },
+    user_() {
+      this.page = this.$route.query.page || 1
+      this.loadObjects()
+    }
+  },
+  methods: {
+    loadObjects() {
+      if (this.loading)
+        return
+      this.loading = true
+      this.objectsApi.findObjects(this.key, this.page - 1, this.size, this.client_.cid, true)
+        .then(res => {
+          this.objects = res.data
+        })
+        .catch(this.$throw)
+        .finally(() => this.loading = false)
+    },
+    newObject() {
 
     }
   },
